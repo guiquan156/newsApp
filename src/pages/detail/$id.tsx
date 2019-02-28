ed9b8be3d88878f012d9ed608b10541c94df3cc5
lian <guiquan156@163.com>
2019-02-28 23:37:00 +0800

    udpate

---
+++ src/pages/detail/$id.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import { connect } from 'dva';
+
+// import BottomMenu from '../../components/BottomMenu';
+// import SearchBar from '../../components/SearchBar';
+
+
+export default class Detail extends React.Component<{}, {}> {
+    render () {
+        return (
+            <p>123</p>
+        );
+    }
+}
+
